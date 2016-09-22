fb912802a228fee0268f9edb9c2e41cb18736574
Michael Leontieff <michael@leontieff.net>
2016-09-22 12:53:20 +1000

    Completed Contact page and modified navigation bar

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,14 +6,14 @@ class Customer < ApplicationRecord
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
   validates :first_name, presence: true, length: {minimum: 2}
   validates :last_name, presence: true, length: {minimum: 2}
-  validates :card_no, presence: true, numericality: true, length: {minimum: 15, maximum: 16} # Amex has 15 digits and Mastercard and Visa have 16 digits
+  validates :card_no, presence: true, length: {minimum: 15, maximum: 16} # Amex has 15 digits and Mastercard and Visa have 16 digits
   validates :card_type, presence: true
-  #validates :ccv, presence, true, numericality: true, length: {minimum: 3, maximum: 3}
+  validates :ccv, presence: true, length: {minimum: 3, maximum: 3}
   validates :card_expiry, presence: true
   validates :street, presence: true
   validates :suburb, presence: true
-  validates :postcode, presence: true, numericality: true, length: {minimum: 4, maximum: 4}
-  validates :city, presence: true, numericality: false
+  validates :postcode, presence: true, length: {minimum: 4, maximum: 4}
+  validates :city, presence: true
   validates :email, presence: true, length: {maximum: 100}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   validates :password, presence: true, length: {minimum: 8}
 
