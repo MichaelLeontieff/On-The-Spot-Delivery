53d2ddf8d79bad2f66e097197b71056d67384af6
Michael Leontieff <michael@leontieff.net>
2016-09-23 18:28:28 +1000

    Added Input Validation created by Amos

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,19 +1,41 @@
+# class Customer < ApplicationRecord
+#   has_many :orders
+#
+#   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+#
+#   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+#   validates :first_name, presence: true, length: {minimum: 2}
+#   validates :last_name, presence: true, length: {minimum: 2}
+#   validates :card_no, presence: true, length: {minimum: 15, maximum: 16} # Amex has 15 digits and Mastercard and Visa have 16 digits
+#   validates :card_type, presence: true
+#   validates :ccv, presence: true, length: {minimum: 3, maximum: 3}
+#   validates :card_expiry, presence: true
+#   validates :street, presence: true
+#   validates :suburb, presence: true
+#   validates :postcode, presence: true, length: {minimum: 4, maximum: 4}
+#   validates :city, presence: true
+#   validates :email, presence: true, length: {maximum: 100}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+#   validates :password, presence: true, length: {minimum: 8}
+#
+#   has_secure_password
+# end
+
 class Customer < ApplicationRecord
   has_many :orders
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
-  validates :first_name, presence: true, length: {minimum: 2}
-  validates :last_name, presence: true, length: {minimum: 2}
-  validates :card_no, presence: true, length: {minimum: 15, maximum: 16} # Amex has 15 digits and Mastercard and Visa have 16 digits
-  validates :card_type, presence: true
-  validates :ccv, presence: true, length: {minimum: 3, maximum: 3}
+  validates :first_name, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/, message: "your first name must only contain letters" }
+  validates :last_name, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/, message: "your last name must only contain letters" }
+  validates :card_no, presence: true, numericality: true, length: {minimum: 15, maximum: 16} # Amex has 15 digits and Mastercard and Visa have 16 digits
+  validates :card_type, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "your card type must only contain letters" }
+  validates :ccv, presence: true, numericality: true, length: {minimum: 3, maximum: 3}
   validates :card_expiry, presence: true
   validates :street, presence: true
-  validates :suburb, presence: true
+  validates :suburb, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "your suburb must only contain letters" }
   validates :postcode, presence: true, length: {minimum: 4, maximum: 4}
-  validates :city, presence: true
+  validates :city, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "your city must only contain letters" }
   validates :email, presence: true, length: {maximum: 100}, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   validates :password, presence: true, length: {minimum: 8}
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,23 @@
+# class Order < ApplicationRecord
+#   has_one :customer, :dependent => :destroy
+#
+#   # User id is injected afterwards, as the currently logged in user places the order and the id is sourced from that
+#   # not user input
+#   #validates :user_id, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+#   VALID_TIME_REGEX = /\A([01]?[0-9]|2[0-3])\:[0-5][0-9]\z/
+#   validates :pickup_time, presence: true, format: {with: VALID_TIME_REGEX}
+#   validates :no_packages, presence: true, numericality: true
+#   validates :gross_weight, presence: true, numericality: true
+#   validates :pickup_time, presence: true
+#   validates :receiver_name, presence: true
+#   validates :receiver_street, presence: true
+#   validates :receiver_suburb, presence: true
+#   validates :receiver_city, presence: true
+#   validates :receiver_postcode, presence: true, numericality: true, length: {minimum: 4, maximum: 4}
+#   validates :insurance, presence: true
+#
+# end
+
 class Order < ApplicationRecord
   has_one :customer, :dependent => :destroy
 
@@ -8,11 +28,10 @@ class Order < ApplicationRecord
   validates :pickup_time, presence: true, format: {with: VALID_TIME_REGEX}
   validates :no_packages, presence: true, numericality: true
   validates :gross_weight, presence: true, numericality: true
-  validates :pickup_time, presence: true
-  validates :receiver_name, presence: true
+  validates :receiver_name, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/, message: "your name must only contain letters" }
   validates :receiver_street, presence: true
-  validates :receiver_suburb, presence: true
-  validates :receiver_city, presence: true
+  validates :receiver_suburb, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "your suburb must contain letters only" }
+  validates :receiver_city, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "your city must contain letters only" }
   validates :receiver_postcode, presence: true, numericality: true, length: {minimum: 4, maximum: 4}
   validates :insurance, presence: true
 
