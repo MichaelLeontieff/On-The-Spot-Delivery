3a1ad320abfc67ddc63b563aae2f3252d4191522
Michael Leontieff <michael@leontieff.net>
2016-10-14 09:02:02 +1100

    Fixed bugs prior to sprint 3 presentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,9 +21,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    session[:driver_id] = nil
-    session[:operator_id] = nil
+    clear_sessions
     flash[:success] = "You have logged out"
     redirect_to root_path
   end
@@ -44,6 +42,7 @@ class SessionsController < ApplicationController
   def auth_customer
     customer = Customer.find_by(email: params[:session][:email])
     if customer && customer.authenticate(params[:session][:password])
+      clear_sessions
       session[:user_id] = customer.id
       flash[:success] = "You have successfully logged in"
       redirect_to main_page_path
@@ -56,6 +55,7 @@ class SessionsController < ApplicationController
   def auth_driver
     driver = Driver.find_by(company_email: params[:session][:email])
     if driver && driver.authenticate(params[:session][:password])
+      clear_sessions
       session[:driver_id] = driver.id
       flash[:success] = "You have successfully logged in"
       redirect_to driver_management_page_path
@@ -68,6 +68,7 @@ class SessionsController < ApplicationController
   def auth_operator
     operator = Operator.find_by(company_email: params[:session][:email])
     if operator && operator.authenticate(params[:session][:password])
+      clear_sessions
       session[:operator_id] = operator.id
       flash[:success] = "You have successfully logged in"
       redirect_to operator_management_page_path
@@ -75,6 +76,13 @@ class SessionsController < ApplicationController
       flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
+  end
+
+  private
+  def clear_sessions
+    session[:user_id] = nil
+    session[:driver_id] = nil
+    session[:operator_id] = nil
   end
 
 end
