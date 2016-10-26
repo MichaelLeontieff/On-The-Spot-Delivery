8d8e026e462fe2a5796def056929c403095d8fc0
Michael Leontieff <michael@leontieff.net>
2016-10-26 12:29:58 +1000

    fixed bugs per acceptance testing

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,7 +1,7 @@
 class Operator < ApplicationRecord
   VALID_NUMBER_PLATE_REGEX =  /[0-9]{1,3}[A-Z]{1,3}/
 
-  validates :company_email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  validates :company_email, presence: true, uniqueness: {case_sensitive: false}
   validates :first_name, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/, message: "your first name must only contain letters" }
   validates :last_name, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/, message: "your last name must only contain letters" }
 
